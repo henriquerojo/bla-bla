f905ab8cc6eecec2dbfbbc713497df1ae5b85c3c
HENRIQUE DUARTE ROJO <henrique.drojo@senacsp.edu.br>
2023-03-31 16:20:19 -0300

    moises

--- TintSysDesk/fmPrincipal.Designer.cs
+++ TintSysDesk/fmPrincipal.Designer.cs
@@ -99,8 +99,10 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(1264, 881);
             this.Controls.Add(this.mspPrincipal);
+            this.ForeColor = System.Drawing.Color.Black;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.IsMdiContainer = true;
             this.MainMenuStrip = this.mspPrincipal;
